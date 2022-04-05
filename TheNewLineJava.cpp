228bdc82468e4779ee0cb81a448a0fc00292d553
abinishae <83404158+abinishae@users.noreply.github.com>
2022-04-05 11:19:36 +0530

    Create TheNewLineJava.cpp

---
+++ TheNewLineJava.cpp
@@ -0,0 +1,25 @@
+
+
+class Geeks{
+    
+   
+    static void printIndividualLine(){
+        
+        
+        System. out. println("Geeks\nfor\nGeeks");
+    }
+    
+}
+
+class GfG{
+    
+    public static void main(String args[]){
+        
+        //Creating an object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling a function of Class Geeks
+        g.printIndividualLine();
+    }
+    
+}  
